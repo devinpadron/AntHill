78d74852e45133a645ccc6493c932d1e4a71372b
Devin Padron <devinpadron@outlook.com>
2024-10-24 22:07:02 -0400

    delete remaining dupes

--- phoneAppBase/src/screens/home/Calendar.tsx
+++ phoneAppBase/src/screens/home/Calendar.tsx
@@ -15,7 +15,7 @@ const leftArrowIcon = require("../../../assets/next.png");
 const rightArrowIcon = require("../../../assets/next.png");
 const ITEMS: any[] = agendaItems;
 
-interface Props {
+interface   s {
   weekView?: boolean;
 }
 
