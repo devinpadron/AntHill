16709ac0db52b8c4f0bf21a7558fb6ce2b05ebcb
williamdougherty <wpdougher@hotmail.com>
2024-10-26 18:34:19 -0400

    removed styles and unnecessary imports in agendaItemController

--- phoneAppBase/src/controller/agendaItemController.ts
+++ phoneAppBase/src/controller/agendaItemController.ts
@@ -1,12 +1,3 @@
-import React, { useCallback, memo } from "react";
-import {
-  StyleSheet,
-  Alert,
-  View,
-  Text,
-  TouchableOpacity,
-  Button,
-} from "react-native";
 import EventController from "./eventController";
 import { MarkedDates } from "react-native-calendars/src/types";
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
@@ -68,59 +59,3 @@ export function getMarkedDates(items: AgendaItemData[]): MarkedDates {
   });
   return marked;
 }
-
-const styles = StyleSheet.create({
-  item: {
-    flexDirection: "row",
-    padding: 16,
-    backgroundColor: "white",
-    borderBottomWidth: 1,
-    borderBottomColor: "#eee",
-    alignItems: "center",
-  },
-  timeContainer: {
-    width: 100,
-    marginRight: 16,
-  },
-  timeRow: {
-    flexDirection: "row",
-  },
-  timeText: {
-    fontSize: 16,
-    fontWeight: "600",
-    color: "#333",
-  },
-  timeSeparator: {
-    color: "#666",
-  },
-  duration: {
-    fontSize: 14,
-    color: "#888",
-    marginTop: 4,
-  },
-  contentContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "500",
-    color: "#333",
-    textAlign: "center",
-  },
-  buttonContainer: {
-    marginLeft: 16,
-  },
-  infoButton: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderRadius: 6,
-    backgroundColor: "#f0f0f0",
-  },
-  infoButtonText: {
-    color: "#666",
-    fontSize: 14,
-    fontWeight: "500",
-  },
-});
