bbfbf767d4babcbca15fdf706e096ca502080d81
Devin Padron <devinpadron@outlook.com>
2025-12-10 13:25:34 -0500

    Update

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
 	expo: {
 		name: "AntHill",
 		slug: "AntHill",
-		version: "1.0.96",
+		version: "1.0.97",
 		orientation: "portrait",
 		userInterfaceStyle: "light",
 		assetBundlePatterns: ["**/*"],
